33de95d2f1dbd8a6fafbd64587d57ac0078b777a
suubcity <suubcity@outlook.com>
2021-04-09 15:05:37 -0700

    Exercises 5.17.-5.22.

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,5 +1,12 @@
 describe('Note app', function () {
 	beforeEach(function () {
+		cy.request('POST', 'http://localhost:3001/api/testing/reset');
+		const user = {
+			name: 'Matti Luukkainen',
+			username: 'mluukkai',
+			password: 'salainen',
+		};
+		cy.request('POST', 'http://localhost:3001/api/users/', user);
 		cy.visit('http://localhost:3000');
 	});
 
@@ -17,12 +24,20 @@ describe('Note app', function () {
 		cy.contains('Matti Luukkainen logged in');
 	});
 
+	it('login fails with wrong password', function () {
+		cy.contains('log in').click();
+		cy.get('#username').type('mluukkai');
+		cy.get('#password').type('wrong');
+		cy.get('#login-button').click();
+
+		cy.get('.error').should('contain', 'wrong credentials');
+
+		cy.get('html').should('not.contain', 'Matti Luukainen logged in');
+	});
+
 	describe('when logged in', function () {
 		beforeEach(function () {
-			cy.contains('log in').click();
-			cy.get('input:first').type('mluukkai');
-			cy.get('input:last').type('salainen');
-			cy.get('#login-button').click();
+			cy.login({ username: 'mluukkai', password: 'salainen' });
 		});
 
 		it('a new note can be created', function () {
@@ -30,6 +45,20 @@ describe('Note app', function () {
 			cy.get('input').type('a note created by cypress');
 			cy.contains('save').click();
 			cy.contains('a note created by cypress');
+		});
+
+		describe('and a note exists', function () {
+			beforeEach(function () {
+				cy.createNote({ content: 'first note', important: false });
+				cy.createNote({ content: 'second note', important: false });
+				cy.createNote({ content: 'third note', important: false });
+			});
+
+			it('other of those can be made important', function () {
+				cy.contains('second note').parent().find('button').as('theButton');
+				cy.get('@theButton').click();
+				cy.get('@theButton').should('contain', 'make not important');
+			});
 		});
 	});
 });
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Note = ({ note, toggleImportance }) => {
-  const label = note.important
-    ? 'make not important' : 'make important'
+	const label = note.important ? 'make not important' : 'make important';
 
-  return (
-    <li className='note'>
-      {note.content}
-      <button onClick={toggleImportance}>{label}</button>
-    </li>
-  )
-}
+	return (
+		<li className="note">
+			<span>{note.content}</span>
+			<button onClick={toggleImportance}>{label}</button>
+		</li>
+	);
+};
 
-export default Note
+export default Note;
